c6caaf85a04ef6e2aacf93bd8b91b888f26aebb5
Sam Prescott <sam@d35.eu>
2018-07-05 11:18:07 +0100

    basic hierarchical graph visualisation implemented

--- HierarchicalGraph.hs
+++ HierarchicalGraph.hs
@@ -9,87 +9,39 @@ import Diagrams.Path
 
 data Graph a = Vertex a | Overlay (Graph a) (Graph a) | Connect (Graph a) (Graph a)
 
-data Grouped = Single String (Diagram B) | Group String [Diagram B] [Grouped] [(String,String)]-- Group Name Diagrams Items Connections
--- data Grouped = Single String (Diagram B) | Group [Grouped] (Maybe (Diagram B))
+inputData = Connect (Connect (Vertex "a") (Overlay (Vertex "b") (Vertex "c"))) (Connect (Vertex "d") (Vertex "e"))
 
-data Part = Part Grouped String 
+layoutPoly :: (V t ~ V2, TrailLike t) => Int -> t
+layoutPoly n = regPoly n 1
 
-inputData = Overlay (Connect (Vertex "a") (Overlay (Vertex "b") (Vertex "c"))) (Connect (Vertex "d") (Vertex "e"))
+node :: String -> Diagram B 
+node n = text n # fontSizeL 0.1 <> circle 0.05 # named n
 
-getGraphName :: Graph a -> a
-getGraphName (Vertex a) = a
--- getGraphName (Connect a b) = getGraphName a ++ getGraphName b
--- getGraphName (Overlay a b) = getGraphName a ++ getGraphName b
-
-
-countGraphVertices :: Graph a -> Int
+countGraphVertices :: Graph String -> Int
 countGraphVertices (Vertex a) = 1
 countGraphVertices (Overlay a b) = countGraphVertices a + countGraphVertices b
 countGraphVertices (Connect a b) = countGraphVertices a + countGraphVertices b
 
+name :: Graph String -> String
+name (Vertex a) = a
+name (Overlay a b) = name a ++ "_overlay_" ++ name b
+name (Connect a b) = name a ++ "_connect_" ++ name b
 
-graphPoly :: (V t ~ V2, TrailLike t) => Int -> t
-graphPoly n = regPoly n 1
-
-node :: String -> String -> Grouped
-node l n = Group l (text l # fontSizeL 0.1 <> circle 0.05 # named l) : [] [] []
-
-drawGraphConnect :: Grouped -> [(String,String)]
-drawGraphConnect (Group _ _ (x:xs) a) = a
-drawGraphConnect (Single _ _) = []
-
-
--- drawGraphConnect :: Grouped -> Grouped -> Grouped
--- drawGraphConnect a@(Single n1 d1) b@(Single n2 d2) = (Group (n1 ++ n2) (connect n1 n2 $ getOverlayDiagram a b) (a:b:[]) )
--- drawGraphConnect a@(Single n1 d1) b@(Group n2 d2 g2) = Group (n1 ++ n2) (connect n1 n2 $ getOverlayDiagram a b) (a : g2)
--- drawGraphConnect a@(Group n1 d1 g2) b@(Single n2 d2) = drawGraphConnect b a
--- drawGraphConnect a@(Group n1 d1 g1) b@(Group n2 d2 g2) = Group (n1 ++ n2) (connect n1 n2 $ getOverlayDiagram a b) (g1 ++ g2)
-
-prepGraphConnect :: Grouped -> Grouped -> Grouped
-prepGraphConnect a@(Single n1 d1) b@(Single n2 d2) = Group (n1 ++ n2) (getOverlayDiagram a b) (a:b:[]) ((n1,n2) : [])
-prepGraphConnect a@(Single n1 d1) b@(Group n2 d2 g2 c2) = Group (n1 ++ n2) (getOverlayDiagram a b) (a : g2) ((n1,n2) : c2)
-prepGraphConnect a@(Group n1 d1 g2 c1) b@(Single n2 d2) = prepGraphConnect b a
-prepGraphConnect a@(Group n1 d1 g1 c1) b@(Group n2 d2 g2 c2) = Group (n1 ++ n2) (getOverlayDiagram a b) (g1 ++ g2) ((n1,n2) : c1 ++ c2)
-
--- drawGraphConnect :: Grouped -> Grouped -> Grouped
--- drawGraphConnect a b = Group name diagram items True
---     where (Group name diagram items _) = drawGraphOverlay a b
-
-getOverlayDiagram :: Grouped -> Grouped -> [Diagram B]
-getOverlayDiagram a b = overlayed
-    where (Group _ overlayed _ _) = drawGraphOverlay a b
-
-drawGraphOverlay :: Grouped -> Grouped -> Grouped
--- drawGraphOverlay a@(Single n1 d1) b@(Single n2 d2) = Group (n1 ++ n2) (a:b:[]) $ Just (hsep 1 [d1, d2])
-drawGraphOverlay a@(Single n1 d1) b@(Single n2 d2) = Group (n1 ++ n2) (d1:d2:[]) (a:b:[]) []
-drawGraphOverlay a@(Single n1 d1) b@(Group n2 d2 g2 c2) = Group (n1 ++ n2) (d1:d2) (a : g2) c2
-drawGraphOverlay a@(Group n1 d1 g2 c1) b@(Single n2 d2) = drawGraphOverlay b a
-drawGraphOverlay a@(Group n1 d1 g1 c1) b@(Group n2 d2 g2 c2) = Group (n1 ++ n2) (d1 ++ d2) (g1 ++ g2) (c1 ++ c2)
-
-
-gCn (Group ) g2 = atPoints vertices1 output1
-    where vertices1 = trailVertices . graphPoly . countGraphVertices $ g1
-          groups@(Group _ output1 _ _) = (getVertices g)
-
-
-makeVertex :: Graph String -> Grouped 
-makeVertex (Vertex a) = node a a
-
-getVertices :: Graph String -> Grouped
-getVertices v@(Vertex a) = makeVertex v
-getVertices (Connect (Vertex a) (Vertex a)) = connect 
-getVertices g@(Connect a b) = prepGraphConnect (getVertices a) (getVertices b)
-    where vertices = trailVertices . graphPoly . countGraphVertices $ g
-          ()
-getVertices (Overlay a b) = drawGraphOverlay (getVertices a) (getVertices b)
-
-
+drawGraph :: Graph String -> Diagram B
+drawGraph g@(Overlay g1@(Vertex a) g2@(Vertex b)) = ((node $ name g1) === strutY 1 === (node $ name g2)) # bgFrame 0.1 red # opacity 0.5 # named (name g)
+drawGraph g@(Overlay g1@(Vertex a) g2) = ((node $ name g1) === strutY 1 === drawGraph g2) # bgFrame 0.1 green # opacity 0.5 # named (name g)
+drawGraph g@(Overlay g1 g2@(Vertex b)) = (drawGraph g1 === strutY 1 === (node $ name g2)) # bgFrame 0.1 yellow # opacity 0.5 # named (name g)
+drawGraph g@(Overlay g1 g2) = (drawGraph g1 === strutY 1 === drawGraph g2) # bgFrame 0.1 blue # opacity 0.5 # named (name g)
+drawGraph g@(Connect g1@(Vertex a) g2@(Vertex b)) = (connectOutside (name g1) (name g2) drawn) # bgFrame 0.1 blue # opacity 0.5 # named (name g)
+    where drawn = (node $ name g1) ||| strutX 1 ||| (node $ name g2)
+drawGraph g@(Connect g1@(Vertex a) g2) = (connectOutside (name g1) (name g2) drawn) # bgFrame 0.1 orange # opacity 0.5 # named (name g)
+    where drawn = (node $ name g1) ||| strutX 1 ||| ((drawGraph g2) # named (name g))
+drawGraph g@(Connect g1 g2@(Vertex b)) = (connectOutside (name g1) (name g2) drawn) # bgFrame 0.1 orange # opacity 0.5 # named (name g)
+    where drawn = (drawGraph g1 # named (name g)) ||| strutX 1 ||| (node $ name g2)
+drawGraph g@(Connect g1 g2) = (connectOutside (name g1) (name g2) drawn) # bgFrame 0.1 orange # opacity 0.5 # named (name g)
+    where drawn = drawGraph g1 ||| strutX 1 ||| drawGraph g2
 
 visualise :: Graph String -> Diagram B
-visualise input = atPoints vertices outputD
-    where vertices = trailVertices . graphPoly . countGraphVertices $ input
-          groups@(Group _ outputD _ _) = (getVertices input)
--- visualise input = atPoints vertices $ map node 
---     where vertices = trailVertices graphPoly $ countGraphVertices input
+visualise g = drawGraph g # frame 0.2
 
 main = mainWith $ visualise inputData
