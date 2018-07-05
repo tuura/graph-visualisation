3a2f9a4dafaf72379945a53a5aca67028ee09237
Sam Prescott <sam@d35.eu>
2018-07-05 14:29:41 +0100

    add colour function as parameter for draw

--- HierarchicalGraph.hs
+++ HierarchicalGraph.hs
@@ -9,7 +9,7 @@ data Graph a = Vertex a | Overlay (Graph a) (Graph a) | Connect (Graph a) (Graph
 
 inputData = Connect (Connect (Vertex "a") (Overlay (Vertex "b") (Vertex "c"))) (Connect (Vertex "d") (Vertex "e"))
 
-colour :: (Ord a, Floating a) => Int -> Colour a
+colour :: Int -> Colour Double
 colour 1 = orange
 colour 2 = blue
 colour 3 = red
@@ -23,12 +23,12 @@ name (Vertex a) = a
 name (Overlay a b) = name a ++ "_overlay_" ++ name b
 name (Connect a b) = name a ++ "_connect_" ++ name b
 
-draw :: Graph String -> Int -> Diagram B
-draw g@(Vertex a) level = node $ name g
-draw g@(Overlay g1 g2) l = (drawn <> boundingRect drawn # fc (colour l) # lw none # opacity 0.4) # named (name g)
-    where drawn = (draw g1 (l + 1) === strutY 1 === draw g2 (l + 1)) # frame 0.2
-draw g@(Connect g1 g2) l = (arrowed <> boundingRect arrowed # fc (colour l) # lw none # opacity 0.4) # named (name g)
+draw :: Graph String -> Int -> (Int -> Colour Double) -> Diagram B
+draw g@(Vertex a) l c = node $ name g
+draw g@(Overlay g1 g2) l c = (drawn <> boundingRect drawn # fc (c l) # lw none # opacity 0.4) # named (name g)
+    where drawn = (draw g1 (l + 1) c === strutY 1 === draw g2 (l + 1) c) # frame 0.2
+draw g@(Connect g1 g2) l c = (arrowed <> boundingRect arrowed # fc (c l) # lw none # opacity 0.4) # named (name g)
     where arrowed = connectOutside (name g1) (name g2) drawn
-          drawn = (draw g1 (l + 1) ||| strutX 1 ||| draw g2 (l + 1)) # frame 0.2
+          drawn = (draw g1 (l + 1) c ||| strutX 1 ||| draw g2 (l + 1) c) # frame 0.2
 
-main = mainWith $ draw inputData 0 # frame 0.2
+main = mainWith $ draw inputData 0 colour # frame 0.2
