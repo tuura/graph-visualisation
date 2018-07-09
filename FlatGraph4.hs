1db571a9bc1fa063a8a70d87b2f1fa979928f7f7
Sam Prescott <sam@d35.eu>
2018-07-09 13:15:51 +0100

    enable loops

--- FlatGraph4.hs
+++ FlatGraph4.hs
@@ -7,6 +7,7 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 import Diagrams.Path
 import Data.Char
+import Data.List
 import Algebra.Graph
 
 data PGraph = PGraph [String] [Diagram B -> Diagram B]
@@ -41,13 +42,14 @@ getVertices s (Connect a b) = PGraph (nA ++ nB) (cA ++ cB ++ ((connectOutside' a
 
 visualise :: (Show a) => Settings -> Graph a -> Diagram B
 visualise s g = mconcat $ connections <*> [diag]
-    where diag = atPoints vertices $ node <$> n
+    where diag = atPoints vertices $ node <$> names
           vertices = trailVertices layout
           layout   = layoutPoly $ countV g
-          (PGraph n connections) = getVertices s g
+          names = nub namesWDup
+          (PGraph namesWDup connections) = getVertices s g
 
 main = mainWith $ visualise (Settings (measureDiv normal $ fromIntegral (countV inputTestData)) (measureDiv thin $ fromIntegral (countV inputTestData))) inputTestData # frame 0.1
 
 -- inputTestData = Connect (Connect (Vertex "a") (Overlay (Vertex "b") (Vertex "c"))) (Connect (Vertex "d") (Vertex "e"))
-inputTestData = (Overlay (Connect (Connect (Connect (Vertex 1) (Connect (Vertex 2) (Vertex 3))) (Vertex 4)) (Overlay (Overlay (Overlay (Vertex 5) (Vertex 6)) (Connect (Connect (Vertex 7) (Connect (Overlay (Connect (Overlay (Connect (Vertex 8) (Connect (Vertex 9) (Vertex 10))) (Vertex 11)) (Vertex 12)) (Vertex 13)) (Vertex 14))) (Vertex 15))) (Overlay (Vertex 16) (Connect (Overlay (Connect (Vertex 17) (Connect (Overlay (Vertex 18) (Vertex 19)) (Vertex 20))) (Vertex 21)) (Overlay (Overlay (Overlay (Vertex 22) (Vertex 23)) (Connect (Connect (Vertex 24) (Vertex 25)) (Vertex 26))) (Vertex 27)))))) (Vertex 28))
+inputTestData = (Overlay (Connect (Connect (Connect (Vertex 1) (Connect (Vertex 2) (Vertex 3))) (Vertex 4)) (Overlay (Overlay (Overlay (Vertex 5) (Vertex 6)) (Connect (Connect (Vertex 7) (Connect (Overlay (Connect (Overlay (Connect (Vertex 7) (Connect (Vertex 9) (Vertex 10))) (Vertex 11)) (Vertex 12)) (Vertex 13)) (Vertex 14))) (Vertex 15))) (Overlay (Vertex 16) (Connect (Overlay (Connect (Vertex 17) (Connect (Overlay (Vertex 18) (Vertex 19)) (Vertex 20))) (Vertex 21)) (Overlay (Overlay (Overlay (Vertex 22) (Vertex 23)) (Connect (Connect (Vertex 24) (Vertex 25)) (Vertex 26))) (Vertex 27)))))) (Vertex 28))
 
