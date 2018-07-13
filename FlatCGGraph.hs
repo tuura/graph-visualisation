0858c15bf9e5d12f994112baf5a5c9c3792a1ce3
Sam Prescott <sam@d35.eu>
2018-07-13 15:39:41 +0100

    got no-cycle graph flat display working

--- FlatCGGraph.hs
+++ FlatCGGraph.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts          #-}
 {-# LANGUAGE TypeFamilies              #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Diagrams.Prelude hiding (union)
 import Diagrams.Backend.SVG.CmdLine
@@ -11,10 +12,12 @@ import Data.List
 import Data.Function
 import Algebra.Graph hiding ((===))
 
-data PGraph = PGraph [String] [(String, String)] deriving (Show)
+data PGraph a = PGraph [a] [(a, a)] deriving (Show)
 
-getVertices :: (Show a) => Graph a -> PGraph
-getVertices (Vertex a) = PGraph [show a] []
+type ConnectList a = [(a,[a])]
+
+getVertices :: (Show a, Eq a) => Graph a -> PGraph a
+getVertices (Vertex a) = PGraph [a] []
 getVertices (Overlay a b) = PGraph (nA ++ nB) (cA ++ cB)
     where (PGraph nA cA) = getVertices a
           (PGraph nB cB) = getVertices b
@@ -22,7 +25,7 @@ getVertices (Connect a b) = PGraph (nA ++ nB) ([(aA, bB) | aA <- nA, bB <- nB] +
     where (PGraph nA cA) = getVertices a
           (PGraph nB cB) = getVertices b
 
-connectedFrom :: [(String,String)] -> [(String,[String])]
+connectedFrom :: (Show a, Eq a) => [(a,a)] -> ConnectList a
 connectedFrom [(a,b),(x,y)]
   | b == y = [(b,[a,x])]
   | otherwise = (b,[a]) : [(y,[x])]
@@ -35,7 +38,7 @@ connectedFrom l@((x,y):zs) = (y,incoming) : if length remaining > 0 then connect
 -- reducedFrom ((x1,ys1):zs1) = foldr (\a acc -> x1 : ) [] ys1
 --     where getCFrom ((x,ys):zs) = foldr (\a acc -> )
 
-connectedTo :: [(String,String)] -> [(String,[String])]
+connectedTo :: (Show a, Eq a) => [(a,a)] -> ConnectList a
 connectedTo [(a,b),(x,y)]
   | a == x = [(a,[b,y])]
   | otherwise = (a,[b]) : [(x,[y])]
@@ -44,15 +47,15 @@ connectedTo l@((x,y):zs) = (x,outgoing) : if length remaining > 0 then connected
           outgoing = foldr (\(a,b) acc -> b : acc) [] filtered
           filtered = filter (\(a,b) -> a == x) l
 
-reduction :: [(String,[String])] -> [(String,[String])] -> [(String,[String])]
+reduction :: (Show a, Eq a) => ConnectList a -> ConnectList a -> ConnectList a
 reduction [] _ = []
 reduction ((x,ys):zs) extras = new : reduction zs (new : extras)
-    where new = (x,foldr (\y acc -> check y acc) ys ys)
+    where new = (x,nub $ foldr (\y acc -> check y acc) ys ys)
           check y accOuter = foldr (\(a,bs) acc -> if y == a then acc \\ (acc `intersect` bs) else acc) accOuter toCheck
           toCheck = extras ++ zs
 
 
-getRoots :: [String] -> [(String,[String])] -> [String]
+getRoots :: (Show a, Eq a) => [a] -> ConnectList a -> [a]
 getRoots n reduced = foldr (\(x,ys) acc -> delete x acc) n reduced
 
 -- -- Topological sorting
@@ -82,56 +85,81 @@ getRoots n reduced = foldr (\(x,ys) acc -> delete x acc) n reduced
 --           roots cTo = getRoots n cTo -- Gets the root nodes from the adjacency list
 
 -- Topological sorting
-getLevelList :: [String] -> [(String,[String])] -> Maybe [String]
-getLevelList [] connTo
-    | length connTo == 0 = Just []
-    | otherwise = Nothing -- Has at least one loop
-getLevelList n connTo = (Just (fst foldRoots)) `mappend` (getLevelList (n \\ roots connTo) $ snd foldRoots)
-    where foldRoots = foldr (\toSort (acc,cTo) -> 
-                            let newList = (toSort : acc) -- Prepend the current root node to the list
-                                folded = foldConnected toSort newList cTo -- Fold over the nodes connected to the root node
-                            in ((fst folded), snd folded)) -- Add the nodes which have no other connections currently to the list
-                            ([],connTo) (roots connTo) -- Start by folding over the root nodes with no incoming connections
-          foldConnected toSort newList cTo = foldr (\b (acc1,acc2) -> -- b is the current node connected to root node a, acc1 is the current list, acc2 is the current adjacency list
-                                      if length (getEdgesTo b acc2) == 0 then (b : acc1, newCTo toSort b acc2) -- If the node b has no more incoming edges it is added to the list and removed from the adjacency list
-                                      else (acc1, newCTo toSort b acc2)) -- Otherwise the list remains the same but it is still removed from the adjacency list
-                                    (newList,cTo) (getEdgesFrom toSort cTo)
-          getEdgesFrom x cTo = foldr (\(a,bs) acc -> if x `elem` bs then a : acc else acc) [] cTo
-          newCTo x r cTo = (x,delete r $ getEdgesTo x cTo) : 
-                            if getEdgesToTuple x cTo /= Nothing then 
-                              let (Just tup) = getEdgesToTuple x cTo 
-                                  in (cTo \\ [tup]) 
-                            else []
-          roots cTo = getRoots n cTo -- Gets the root nodes from the adjacency list
+getLevelList :: (Show a, Eq a) => [a] -> ConnectList a -> [a]
+getLevelList [] _ = []
+getLevelList q firstCTo = let (queue,list,cTo) = foldThroughRoots q firstCTo
+                              in list ++ (getLevelList queue cTo)
+    -- where recurse = (Just (fst foldRoots)) `mappend` (getLevelList (n \\ roots connTo) $ snd foldRoots)
+          
+foldThroughRoots :: (Show a, Eq a) => [a] -> ConnectList a -> ([a], [a], ConnectList a)
+foldThroughRoots queue firstCTo = foldr (\root (accQueue,accList,accCTo) -> 
+                                    let newList = (accList ++ [root]) -- Prepend the current root node to the list
+                                        folded = foldThroughConnectedNodes root accQueue accCTo -- Fold over the nodes connected to the root node
+                                    in (fst folded, newList, snd folded)) -- Add the nodes which have no other connections currently to the list
+                                    ([],[],firstCTo) queue -- Start by folding over the root nodes with no incoming connections
 
-dependsOn :: String -> String -> [(String,[String])] -> Bool
+
+foldThroughConnectedNodes :: (Show a, Eq a) => a -> [a] -> ConnectList a -> ([a], ConnectList a)
+foldThroughConnectedNodes root queue cTo = foldr (\b (accQueue,acc2) -> -- b is the current node connected to root node a, acc1 is the current list, acc2 is the current adjacency list
+                                              if length (getEdgesTo b acc2) <= 1 then (accQueue ++ [b], deleteConnection root b acc2) -- If the node b has no more incoming edges it is added to the list and removed from the adjacency list
+                                              else (accQueue, deleteConnection root b acc2)) -- Otherwise the list remains the same but it is still removed from the adjacency list
+                                              (queue,cTo) (getEdgesFrom root cTo)
+
+deleteConnection :: (Show a, Eq a) => a -> a -> ConnectList a -> ConnectList a
+deleteConnection x r cTo = (\(a,bs) -> (a,delete x bs)) <$> cTo
+
+-- deleteConnection x r cTo = (x,delete r $ getEdgesFrom x cTo) : 
+-- if getEdgesToTuple x cTo /= Nothing then 
+--   let (Just tup) = getEdgesToTuple x cTo 
+--       in (cTo \\ [tup])
+-- else []
+
+dependsOn :: (Show a, Eq a) => a -> a -> ConnectList a -> Bool
 dependsOn a b cTo = b `elem` (getEdgesTo a cTo) -- If a depends on b
 
-getEdgesTo :: String -> [(String,[String])] -> [String]
+getEdgesTo :: (Show a, Eq a) => a -> ConnectList a -> [a]
 getEdgesTo x cTo = if getEdgesToTuple x cTo /= Nothing then let (Just tup) = getEdgesToTuple x cTo in snd tup else []
-getEdgesToTuple :: String -> [(String,[String])] -> Maybe (String,[String])
+
+getEdgesToTuple :: (Show a, Eq a) => a -> ConnectList a -> Maybe (a,[a])
 getEdgesToTuple x cTo = find (\(a,bs) -> a == x) cTo
 
-getLevels :: [String] -> [(String,[String])] -> [[String]]
+getEdgesFrom :: (Show a, Eq a) => a -> ConnectList a -> [a]
+getEdgesFrom x cTo = foldr (\(a,bs) acc -> if x `elem` bs then a : acc else acc) [] cTo
+
+-- getLevels :: (Show a, Eq a) => [a] -> [(a,[a])] -> [[a]]
 getLevels [] _ = []
 getLevels l cTo = foldLevel : (getLevels (l \\ foldLevel) cTo)
-    where foldLevel = fst $ foldr (\x (acc,flag) -> if flag || (or $ zipWith (x `dependsOn`) acc (repeat cTo)) then (acc,True) else (x:acc,False)) ([],False) l
+    where foldLevel = fst $ foldr (\x (acc,flag) -> if flag || (or $ zipWith (x `dependsOn`) acc (repeat cTo)) || (or $ map (\a -> dependsOn a x cTo) acc) then (acc,True) else (x:acc,False)) ([],False) l
 
-reducedConnections :: [(String,[String])] -> [(String,String)]
+reducedConnections :: (Show a, Eq a) => ConnectList a -> [(a,a)]
 reducedConnections l = foldr (\(x,ys) acc -> (zip ys (repeat x)) ++ acc) [] l
 
 node :: String -> Diagram B
 node n = (text n # fontSizeL 0.1 # href ("javascript:alert(\"Node " ++ n ++ "\")") <> circle 0.1) # named n
 
+layerDifference :: (Show a, Eq a) => a -> a -> [[a]] -> Int
+layerDifference a b l = (foldLayers a) - (foldLayers b)
+    where foldLayers x = snd (foldr (\xs (c1,c2) -> if x `elem` xs then (c1,c1) else (c1 + 1,c2)) (0,0) l)
+
+visualiseLayers :: [[String]] -> Diagram B
+visualiseLayers levelled = vsep 0.2 $ foldl (\acc level -> (center (hsep 0.3 $ node <$> level)) : acc) [] levelled
+
 visualise :: Diagram B
 visualise = mconcat connected
-    where connected = zipWith (\a b -> connectOutside a b unconnected) (fst <$> (reducedConnections reduced)) (snd <$> (reducedConnections reduced))
-          unconnected = vsep 0.2 $ foldr (\level acc -> (center (hsep 0.3 $ node <$> level)) : acc) [] leveled
-          leveled = getLevels topList reduced
-          topList = if sortedListMaybe /= Nothing then let (Just sortedList) = sortedListMaybe in reverse sortedList else []
-          sortedListMaybe = getLevelList n reduced
+    -- where connected = zipWith (\a b -> (if (layerDifference a b levelled) > 1 then arrows arrowOpts2 a b else arrows arrowOpts1 a b)) (fst <$> finalConnections) (snd <$> finalConnections)
+    where connected = map (\(a,b) -> (if abs (layerDifference a b levelled) > 1 then arrows arrowOpts2 a b else arrows arrowOpts1 a b)) finalConnections
+          arrows aO a b = connectPerim' aO a b (-1/4 @@ turn) (1/4 @@ turn) $ visualiseLayers levelled
+          arrowOpts1 = with
+          arrowOpts2 = with & arrowShaft .~ (arc xDir (3/12 @@ turn))
+          finalConnections = reducedConnections reduced
+          levelled = getLevels topList reduced 
+          -- topList :: (Show a, Eq a) => [a]
+          topList = getLevelList (getRoots n reduced) reduced
           reduced = reduction (connectedFrom connections) []
+          -- n :: (Show a, Eq a) => [a]
           n = nub names
+
+
           (PGraph names connections) = getVertices g
           g = inputTestData
 
@@ -143,4 +171,6 @@ main = mainWith $ visualise # frame 0.1
 
 -- main = mainWith $ visualise inputTestData # frame 0.1
 
-inputTestData = (Connect (Connect (Connect (Connect (Vertex 1) (Connect (Vertex 2) (Vertex 3))) (Vertex 4)) (Overlay (Overlay (Overlay (Vertex 5) (Vertex 6)) (Connect (Connect (Vertex 7) (Connect (Overlay (Connect (Overlay (Connect (Vertex 8) (Connect (Vertex 9) (Vertex 10))) (Vertex 11)) (Vertex 12)) (Vertex 13)) (Vertex 14))) (Vertex 21))) (Overlay (Vertex 16) (Connect (Overlay (Connect (Vertex 17) (Connect (Overlay (Vertex 18) (Vertex 19)) (Vertex 20))) (Vertex 15)) (Overlay (Overlay (Overlay (Vertex 22) (Vertex 23)) (Connect (Connect (Vertex 24) (Vertex 25)) (Vertex 26))) (Vertex 27)))))) (Vertex 28))
+-- inputTestData = (Connect (Connect (Connect (Connect (Vertex 1) (Connect (Vertex 2) (Vertex 3))) (Vertex 4)) (Overlay (Overlay (Overlay (Vertex 5) (Vertex 6)) (Connect (Connect (Vertex 7) (Connect (Overlay (Connect (Overlay (Connect (Vertex 8) (Connect (Vertex 9) (Vertex 10))) (Vertex 11)) (Vertex 12)) (Vertex 13)) (Vertex 14))) (Vertex 21))) (Overlay (Vertex 16) (Connect (Overlay (Connect (Vertex 17) (Connect (Overlay (Vertex 18) (Vertex 19)) (Vertex 20))) (Vertex 15)) (Overlay (Overlay (Overlay (Vertex 22) (Vertex 23)) (Connect (Connect (Vertex 24) (Vertex 25)) (Vertex 26))) (Vertex 27)))))) (Vertex 28))
+inputTestData :: Graph String
+inputTestData = fmap show $ Connect (Vertex 1) (Overlay (Connect (Vertex 2) (Overlay (Connect (Vertex 4) (Vertex 7)) (Connect (Vertex 5) (Vertex 7)))) (Connect (Vertex 3) (Connect (Vertex 6) (Connect (Vertex 5) (Vertex 7)))))
