cf23faf458b202c44c7515fe99112f0dfa5c5e21
Sam Prescott <sam@d35.eu>
2018-07-15 14:21:47 +0100

    extract common functions

--- src/Visualise.hs
+++ src/Visualise.hs
@@ -1,14 +1,24 @@
 module Visualise (
     ProcessedGraph(..),
 
-    countVertices, getVertices, dynamicStyle
+    Dimensions, ConnectList,
+
+    draw, countVertices, getVertices, connectedFrom, connectedTo, dynamicStyle
 ) where
 
 import Algebra.Graph
 import Diagrams.Prelude
 import Diagrams.Backend.SVG
+import Data.List
 
 data ProcessedGraph a = ProcessedGraph [a] [(a, a)] deriving (Show)
+
+type Dimensions = (Maybe Double, Maybe Double)
+
+type ConnectList a = [(a,[a])]
+
+draw :: FilePath -> Dimensions -> Diagram SVG -> IO ()
+draw path (w,h) = renderSVG path (mkSizeSpec2D w h)
 
 countVertices :: Graph a -> Measure Double
 countVertices (Vertex a) = 1
@@ -24,6 +34,25 @@ getVertices (Connect a b) = ProcessedGraph (nA ++ nB) ([(aA, bB) | aA <- nA, bB 
     where (ProcessedGraph nA cA) = getVertices a
           (ProcessedGraph nB cB) = getVertices b
 
+connectedFrom :: (Show a, Eq a) => [(a,a)] -> ConnectList a
+connectedFrom [(a,b),(x,y)]
+  | b == y = [(b,[a,x])]
+  | otherwise = (b,[a]) : [(y,[x])]
+connectedFrom l@((x,y):zs) = (y,incoming) : if not (null remaining) then connectedFrom remaining else []
+    where remaining = zs \\ filtered
+          incoming = foldr (\(a,b) acc -> a : acc) [] filtered
+          filtered = filter (\(a,b) -> b == y) l
+
+connectedTo :: (Show a, Eq a) => [(a,a)] -> ConnectList a
+connectedTo [(a,b),(x,y)]
+  | a == x = [(a,[b,y])]
+  | otherwise = (a,[b]) : [(x,[y])]
+connectedTo l@((x,y):zs) = (x,outgoing) : if not (null remaining) then connectedTo remaining else []
+    where remaining = zs \\ filtered
+          outgoing = foldr (\(a,b) acc -> b : acc) [] filtered
+          filtered = filter (\(a,b) -> a == x) l
+
 
 dynamicStyle :: Measure Double -> Measure Double -> Measure Double
 dynamicStyle def graphSize = def * 10/graphSize
+
