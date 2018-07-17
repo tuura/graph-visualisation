11b9808f9bf3c05f8183d05ef510e691a93d2464
Sam Prescott <sam@d35.eu>
2018-07-17 15:43:34 +0100

    improve arrows and level ordering

--- src/Visualise/DAG.hs
+++ src/Visualise/DAG.hs
@@ -7,7 +7,7 @@
 module Visualise.DAG (
     Settings,
 
-    drawDAG, drawDAG', drawDAGPartialOrder, drawDAGPartialOrder'
+    drawDAG, drawDAG'--, drawDAGPartialOrder, drawDAGPartialOrder'
 ) where
 
 import Visualise
@@ -21,6 +21,8 @@ import Data.Maybe
 data Settings = Settings { dynamicHead :: Measure Double
                          , dynamicThick :: Measure Double
                      }
+
+data LayerPosition = LayerLeft | LayerMiddle | LayerRight deriving (Eq)
 
 reduction :: (Show a, Eq a) => ConnectList a -> ConnectList a -> ConnectList a
 reduction [] _ = []
@@ -37,7 +39,7 @@ getRoots = foldr (\(x,ys) acc -> delete x acc)
 getLevelList :: (Show a, Eq a) => [a] -> ConnectList a -> [a]
 getLevelList [] _ = []
 getLevelList q firstCTo = let (queue,list,cTo) = foldThroughRoots q firstCTo
-                              in list ++ getLevelList queue cTo
+                          in list ++ getLevelList queue cTo
           
 foldThroughRoots :: (Show a, Eq a) => [a] -> ConnectList a -> ([a], [a], ConnectList a)
 foldThroughRoots queue firstCTo = foldr (\root (accQueue,accList,accCTo) -> 
@@ -68,11 +70,19 @@ getEdgesToTuple x = find (\(a,bs) -> a == x)
 getEdgesFrom :: (Show a, Eq a) => a -> ConnectList a -> [a]
 getEdgesFrom x = foldr (\(a,bs) acc -> if x `elem` bs then a : acc else acc) []
 
-getLevels :: (Show a, Eq a) => [a] -> [(a,[a])] -> [[a]]
-getLevels [] _ = []
-getLevels l cTo = foldLevel : getLevels (l \\ foldLevel) cTo
-    where foldLevel = fst $ foldr (\x (acc,flag) -> if flag || or ((\a -> dependsOn x a cTo) <$> acc) || any (\a -> dependsOn a x cTo) acc 
-                                                    then (acc,True) else (x:acc,False)) ([],False) (reverse l)
+getLevels :: (Show a, Eq a) => [a] -> [a] -> [(a,[a])] -> [[a]]
+getLevels [] _ _ = []
+getLevels l lastLevel cTo = foldLevel : getLevels (l \\ foldLevel) foldLevel cTo
+    where foldLevel = let (left,right,_) = foldr (\x (accL,accR,flag) -> if flag || moveToNext x accL accR then (accL,accR,True) 
+                                                                         else case nodePosition x of LayerMiddle -> (accL ++ [x], accR, False)
+                                                                                                     LayerRight  -> (accL, x : accR, False)
+                                                                                                     LayerLeft   -> (x:accL, accR, False)
+                                           ) ([],[],False) (reverse l)
+                      in left ++ (reverse right)
+          nodePosition x = foldr (\a acc -> if (x `dependsOn` a) cTo then a `positionInGivenLayer` lastLevel else acc) LayerMiddle lastLevel
+          moveToNext x accL accR = or ((\a -> dependsOn x a cTo) <$> accL) || or ((\a -> dependsOn x a cTo) <$> accR) || any (\a -> dependsOn a x cTo) accL || any (\a -> dependsOn a x cTo) accR
+
+
 
 reducedConnections :: (Show a, Eq a) => ConnectList a -> [(a,a)]
 reducedConnections = foldr (\(x,ys) acc -> zip ys (repeat x) ++ acc) []
@@ -80,38 +90,61 @@ reducedConnections = foldr (\(x,ys) acc -> zip ys (repeat x) ++ acc) []
 layerDiff :: (Show a, Eq a) => a -> a -> [[a]] -> Int
 layerDiff a b l = fst (foldGraphLayers a l) - fst (foldGraphLayers b l)
 
-isElemOnLeft :: (Show a, Eq a) => a -> [[a]] -> Bool
-isElemOnLeft x l = snd $ foldGraphLayers x l
+elemPosition :: (Show a, Eq a) => a -> [[a]] -> LayerPosition
+elemPosition x l = snd $ foldGraphLayers x l
 
-foldGraphLayers :: (Show a, Eq a) => a -> [[a]] -> (Int,Bool)
+foldGraphLayers :: (Show a, Eq a) => a -> [[a]] -> (Int,LayerPosition)
 foldGraphLayers x l = let ((_,layerNum), isLeft) = fold in (layerNum, isLeft)
-    where fold = foldr (\xs ((c1,c2),isLeft) -> if x `elem` xs then ((c1,c1), isOnLeftOfLayer (getElemIndex $ x `elemIndex` xs) (length xs - 1))
-                                                else ((c1 + 1,c2),isLeft)) ((0,0),False) l
-          getElemIndex (Just i) = i
-          getElemIndex Nothing = 0
+    where fold = foldr (\xs ((c1,c2),isLeft) -> if x `elem` xs then ((c1,c1), positionInGivenLayer x xs)
+                                                else ((c1 + 1,c2),isLeft)) ((0,0),LayerMiddle) l
 
-isOnLeftOfLayer :: Int -> Int -> Bool
-isOnLeftOfLayer x y
-    | fromIntegral x < fromIntegral y/2 = True
-    | otherwise = False
+positionInGivenLayer :: (Eq a) => a -> [a] -> LayerPosition
+positionInGivenLayer x ys
+    | position == midPoint = LayerMiddle
+    | position < midPoint = LayerLeft
+    | otherwise = LayerRight
+        where midPoint = fromIntegral (length ys - 1)/2
+              position = fromIntegral (getElemIndex $ x `elemIndex` ys)
+              getElemIndex (Just i) = i
+              getElemIndex Nothing = 0
 
 visualiseLayers :: [[String]] -> Diagram SVG
 visualiseLayers levelled = vsep 0.2 $ foldl (\acc level -> center (hsep 0.3 $ node 0.1 0.1 <$> level) : acc) [] levelled
 
-connectNodes :: ArrowOpts Double -> String -> String -> [[String]] -> Maybe Bool -> Diagram B
-connectNodes arrowOptsF n1 n2 levelled isLeft = connectPerim' arrowOptsF n1 n2 degreeOne degreeTwo $ visualiseLayers levelled
-    where degreeOne = getArrowPoints isLeft (1/2 @@ turn) (0 @@ turn) (-1/4 @@ turn)
-          degreeTwo = getArrowPoints isLeft (1/2 @@ turn) (0 @@ turn) (1/4 @@ turn)
+connectNodes :: ArrowOpts Double -> String -> String -> [[String]] -> Maybe LayerPosition -> Diagram B
+connectNodes arrowOptsF n1 n2 levelled pos = connectPerim' arrowOptsF n1 n2 degreeOne degreeTwo $ visualiseLayers levelled
+    where degreeOne = getArrowPoints pos (1/2 @@ turn) (0 @@ turn) (-1/4 @@ turn)
+          degreeTwo = getArrowPoints pos (1/2 @@ turn) (0 @@ turn) (1/4 @@ turn)
 
-getArrowPoints :: Maybe Bool -> a -> a -> a -> a
-getArrowPoints isLeft a b c = if isJust isLeft then let (Just left) = isLeft in if left then a else b else c
+getArrowPoints :: Maybe LayerPosition -> a -> a -> a -> a
+getArrowPoints posM a b c = if isJust posM 
+                            then let (Just pos) = posM 
+                                 in if pos == LayerLeft then a 
+                                    else if pos == LayerRight then b
+                                         else c 
+                            else c
+
+visualiseDAG :: Settings -> [String] -> [(String,String)] -> ConnectList String -> Diagram B
+visualiseDAG s names rawConnections connectedList = mconcat connectedDiagram # frame 0.1
+    where connectedDiagram = map (\(a,b) -> (if abs (layerDiff a b levelled) > 1 
+                                             then connectNodes (arrowOpts1 & (arrowOpts2 a b)) a b levelled $ Just (elemPosition a levelled) 
+                                             else connectNodes arrowOpts1 a b levelled Nothing)) rawConnections
+          arrowOpts1 = with & headLength .~ dynamicHead s & shaftStyle %~ lw (dynamicThick s)
+          arrowOpts2 a b = let posA = elemPosition a levelled 
+                               posB = elemPosition b levelled
+                         in if posA == LayerLeft || (posA == LayerMiddle && posB == LayerLeft) 
+                            then arrowShaft .~ arc xDir (3/12 @@ turn) 
+                            else arrowShaft .~ arc xDir (-3/12 @@ turn)
+          levelled = reverse . getLevels topList [] $ connectedList
+          topList = reverse . nub . reverse $ getLevelList (getRoots names connectedList) connectedList
 
 drawDAGPartialOrder :: (Show a) => Graph a -> Diagram B
 drawDAGPartialOrder g = drawDAGPartialOrder' (defaultSettings g) g
 
 drawDAGPartialOrder' :: (Show a) => Settings -> Graph a -> Diagram B
-drawDAGPartialOrder' s graph = visualiseDAG s names connections connectionList
-    where connectionList = reduction (connectedFrom connections) []
+drawDAGPartialOrder' s graph = visualiseDAG s names newConnections reduced
+    where newConnections = reducedConnections reduced
+          reduced = reduction (connectedFrom connections) []
           names = nub namesWDuplicates
           connections = nub connectionsWDuplicates
           (ProcessedGraph namesWDuplicates connectionsWDuplicates) = getVertices g
@@ -121,23 +154,12 @@ drawDAG :: (Show a) => Graph a -> Diagram B
 drawDAG g = drawDAG' (defaultSettings g) g
 
 drawDAG' :: (Show a) => Settings -> Graph a -> Diagram B
-drawDAG' s graph = visualiseDAG s names connections connectionList
-    where connectionList = connectedFrom connections
+drawDAG' s graph = visualiseDAG s names connections connectedList
+    where connectedList = connectedFrom connections
           names = nub namesWDuplicates
-          connections = nub connectionsWDuplicates
+          connections = nub . reverse $ connectionsWDuplicates
           (ProcessedGraph namesWDuplicates connectionsWDuplicates) = getVertices g
           g = show <$> graph
-
-visualiseDAG :: Settings -> [String] -> [(String,String)] -> ConnectList String -> Diagram B
-visualiseDAG s names rawConnections connectionList = mconcat connectedDiagram # frame 0.1
-    where connectedDiagram = map (\(a,b) -> (if abs (layerDiff a b levelled) > 1 
-                                             then connectNodes (arrowOpts2 a) a b levelled $ Just (isLeft a) 
-                                             else connectNodes arrowOpts1 a b levelled Nothing)) rawConnections
-          arrowOpts1 = with & headLength .~ dynamicHead s & shaftStyle %~ lw (dynamicThick s)
-          arrowOpts2 a = arrowOpts1 & if isLeft a then arrowShaft .~ arc xDir (3/12 @@ turn) else arrowShaft .~ arc xDir (-3/12 @@ turn)
-          isLeft a = isElemOnLeft a levelled
-          levelled = reverse . getLevels topList $ connectionList 
-          topList = nub $ getLevelList (getRoots names connectionList) connectionList
 
 defaultSettings :: Graph a -> Settings
 defaultSettings g = Settings (dynamicStyle normal $ countVertices g) 
