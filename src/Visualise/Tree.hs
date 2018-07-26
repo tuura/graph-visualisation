fc9b49451371fa4f40beeda3790f3fbbba8fdb6b
Sam Prescott <sam@d35.eu>
2018-07-26 11:37:25 +0100

    fix using connectedFrom instead of connectedTo

--- src/Visualise/Tree.hs
+++ src/Visualise/Tree.hs
@@ -38,7 +38,8 @@ reduction ((x,ys):zs) extras = new : reduction zs (new : extras)
 getRoots :: (Eq a) => [a] -> ConnectList a -> [a]
 getRoots = foldr (\(x,ys) acc -> delete x acc)
 
--- Topological sorting
+-- | Topological sorting of a graph represented by a list of vertices and a 'ConnectList' of vertex connections.
+-- It sorts the graph by using Kahn's algorithm, producing an ordered list of vertices.
 getLevelList :: (Eq a) => [a] -> ConnectList a -> [a]
 getLevelList [] _ = []
 getLevelList q firstCTo = let (queue,list,cTo) = foldThroughRoots q firstCTo
@@ -86,7 +87,7 @@ getLevels l lastLevel cTo = foldLevel : getLevels (l \\ foldLevel) foldLevel cTo
           moveToNext x accL accR = or ((\a -> dependsOn x a cTo) <$> accL) || or ((\a -> dependsOn x a cTo) <$> accR) || any (\a -> dependsOn a x cTo) accL || any (\a -> dependsOn a x cTo) accR
 
 reducedConnections :: ConnectList a -> [(a,a)]
-reducedConnections = reverse . foldr (\(x,ys) acc -> zip ys (repeat x) ++ acc) []
+reducedConnections = foldr (\(x,ys) acc -> zip (repeat x) ys ++ acc) []
 
 layerDiff :: (Eq a) => a -> a -> [[a]] -> Int
 layerDiff a b l = fst (foldGraphLayers a l) - fst (foldGraphLayers b l)
@@ -149,8 +150,8 @@ visualiseTree s nodes rawConnections connectedList = outDiag <> boundingRect out
                          in if posA == LayerLeft || (posA == LayerMiddle && posB == LayerLeft) 
                             then arrowShaft .~ arc xDir (3/12 @@ turn) 
                             else arrowShaft .~ arc xDir (-3/12 @@ turn)
-          levelled = reverse . getLevels topList [] $ connectedList
-          topList = reverse . nub . reverse $ getLevelList (getRoots nodes connectedList) connectedList
+          levelled = getLevels topList [] $ connectedList
+          topList = nub $ getLevelList (getRoots nodes connectedList) connectedList
 
 drawTreePartialOrder :: (Show a, Eq a, Countable a) => Graph a -> Diagram B
 drawTreePartialOrder = drawTreePartialOrder' defaultTreeSettings drawDefaultNode
@@ -158,7 +159,7 @@ drawTreePartialOrder = drawTreePartialOrder' defaultTreeSettings drawDefaultNode
 drawTreePartialOrder' :: (Show a, Eq a, Countable a) => (Graph a -> Settings) -> (a -> Diagram B) -> Graph a -> Diagram B
 drawTreePartialOrder' settingsF drawF g = visualiseTree s nodes newConnections reduced
     where newConnections = reducedConnections reduced
-          reduced = reduction (connectedFrom connections) []
+          reduced = reduction (connectedTo connections) []
           (ProcessedGraph nodes connections) = getVertices drawF g
           s = settingsF g
 
@@ -166,8 +167,8 @@ drawTree :: (Show a, Eq a, Countable a) => Graph a -> Diagram B
 drawTree = drawTree' defaultTreeSettings drawDefaultNode
 
 drawTree' :: (Show a, Eq a, Countable a) => (Graph a -> Settings) -> (a -> Diagram B) -> Graph a -> Diagram B
-drawTree' settingsF drawF g = visualiseTree s nodes (reverse connections) connectedList
-    where connectedList = connectedFrom (reverse connections)
+drawTree' settingsF drawF g = visualiseTree s nodes connections connectedList
+    where connectedList = connectedTo connections
           (ProcessedGraph nodes connections) = getVertices drawF g
           s = settingsF g
 
